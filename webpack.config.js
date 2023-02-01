602fe00e573efeb06a0c698d33b0bd9510405565
Tomás Leggire <tomasleggire@hotmail.com>
2023-02-01 20:51:11 -0300

    React con CSS y Sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -28,12 +29,30 @@ module.exports = {
                     }
                 ]
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+          },
+        compress: true,
+        port: 3005,
+      }
 }
