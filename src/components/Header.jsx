f0fa94aee424d90ac104f81053d5128fd8e93c16
Tomás Leggire <tomasleggire@hotmail.com>
2023-02-15 19:51:53 -0300

    Orden de compra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React, { useContext, useState } from 'react';
 import '@styles/Header.scss';
 import Menu from './Menu';
+import MyOrder from '../containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 import AppContext from '../context/AppContext';
 
 const Header = () => {
-
 	const { state } = useContext(AppContext);
 
 	const [toggle, setToggle] = useState(false);
+	const [toggleOrders, setToggleOrders] = useState(false);
+
 	const handleToggle = () => {
 		setToggle(!toggle);
 	}
@@ -46,13 +48,14 @@ const Header = () => {
 					<li className="navbar-email" onClick={handleToggle}>
 						platzi@example.com
 					</li>
-					<li className="navbar-shopping-cart">
+					<li className="navbar-shopping-cart" onClick={()=>setToggleOrders(!toggleOrders)}>
 						<img src={shoppingCart} alt="shopping cart" />
 						{state.cart.length ? <div>{state.cart.length}</div> : null}
 					</li>
 				</ul>
 			</div>
 			{toggle ? <Menu /> : ''}
+			{toggleOrders ? <MyOrder /> : ''}
 		</nav>
 	);
 }
