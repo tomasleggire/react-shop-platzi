999b60f5d9fa5726beb20b373759f5cc3c800bb8
Tomás Leggire <tomasleggire@hotmail.com>
2023-02-20 16:44:42 -0300

    Eliminando productos del carrito

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../styles/OrderItem.scss';
 import close from '../assets/icons/icon_close.png';
+import AppContext from '../context/AppContext';
+
 
 const OrderItem = ({ product }) => {
+
+	const { removeFromCart } = useContext(AppContext);
+
 	return (
 		<div className="OrderItem">
 			<figure>
@@ -10,7 +15,7 @@ const OrderItem = ({ product }) => {
 			</figure>
 			<p>{product.title}</p>
 			<p>${product.price}</p>
-			<img src={close} alt="close" />
+			<img src={close} alt="close" onClick={() => removeFromCart(product)}/>
 		</div>
 	);
 }
